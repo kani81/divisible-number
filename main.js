21c16db706fd86b8133e4f4a9802c80251c947e3
kani81 <kaniarasiduraisamy.com>
2023-07-19 15:38:20 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,9 @@
+var arr=[15,10,70,3,18];
+document.write("arr=[15,10,70,3,18]"+"<br>"+"<br>");
+for(let i=0;i<arr.length;i++){
+	
+		if((arr[i]%3==0)&&(arr[i]%5==0)){
+			
+					document.write(arr[i]);
+				}
+			}
